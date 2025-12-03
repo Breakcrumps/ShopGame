da98717d8a57ffb997d7b022c19bf85344dd661b
Breakcrumps <sonickita7@gmail.com>
2025-12-03 09:57:03 +0300

    Big change to shelf logic incoming

---
+++ Characters/FieldGirl.cs
@@ -0,0 +1,22 @@
+using Godot;
+
+namespace ShopGame.UI;
+
+[GlobalClass]
+internal sealed partial class FieldGirl : CharacterBody2D
+{
+  [Export] private float _walkSpeed = 150f;
+  [Export] private float _runSpeed = 250f;
+  
+  public override void _PhysicsProcess(double delta)
+  {
+    float speed = Input.IsActionPressed("Run") ? _runSpeed : _walkSpeed;
+
+    Velocity = new(Input.GetAxis("Left", "Right") * speed, GetVertVelocity());
+
+    MoveAndSlide();
+  }
+
+  private float GetVertVelocity()
+    => IsOnFloor() ? 0f : Velocity.Y + 10f;
+}
